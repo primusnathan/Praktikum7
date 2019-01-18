e49fe13e9bf34049c288337fda038533bde8b185
primusnathan <primus_nathan_26rpl@student.smktelkom-mlg.sch.id>
2019-01-18 19:20:24 +0700

    selesai

---
+++ application/views/data_diri.php
@@ -0,0 +1,38 @@
+<h1>Biodata</h1>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2>Data Diri</h2>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+                        <div class="body">
+                        	<div class="row">
+                            <div class="col-md-4">
+                            	<img style="width: 50%" src="<?=base_url('assets/images/sum.jpg')?>">
+                            </div>
+                            <div class="col-md-8">
+                            	<table class="table table-hover table-striped">
+                            		<tr>
+                            			<td>Nama</td><td>: Primus Nathan Orvala </td>
+                            		</tr>
+                            	 	<tr>
+                            			<td>Kelas</td><td>: XIR2 </td>
+                            		</tr>
+                            		<tr>
+                            			<td>Umur</td><td>: 16 thn </td>
+                            		</tr>
+                            		<tr>
+                            			<td>Alamat</td><td>: Malang </td>
+                            		</tr>
+                            	
+                            	</table>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
